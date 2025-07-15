d9a54b9ebedce05925ae40a336648f6f43805cb2
fiadaze <fiakantapon@gmail.com>
2025-07-16 01:04:06 +0700

    added sumrange formula

--- sumRange.js
+++ sumRange.js
@@ -2,3 +2,9 @@ function sumRange(num) {
   if (num === 1) return 1;
   return num + sumRange(num - 1);
 }
+
+//this shit is better than recursive for this function
+
+function sumRange(num) {
+  return (num * (num + 1)) / 2;
+}
