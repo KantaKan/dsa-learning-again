796ae52036349888acd960de6e64dfd856696afc
fiadaze <fiakantapon@gmail.com>
2025-07-16 23:43:44 +0700

    added fib again

--- pureRecursion.js
+++ pureRecursion.js
@@ -26,3 +26,10 @@ function productOfArray(arr) {
 
   return arr[0] * productOfArray(arr.slice(1));
 }
+
+function fib(n) {
+  if (n === 1) return 1;
+  if (n === 0) return 0;
+
+  return fib(n - 1) + fib(n - 2);
+}
