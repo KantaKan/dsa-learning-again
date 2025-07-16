f6eb932101f975dea06e969f3b714a87780b1782
fiadaze <fiakantapon@gmail.com>
2025-07-16 23:31:04 +0700

    added some recursive with arr slice

--- pureRecursion.js
+++ pureRecursion.js
@@ -20,3 +20,9 @@ function power(num, exp) {
 
   return num * power(num, exp - 1);
 }
+
+function productOfArray(arr) {
+  if (arr.length === 0) return 1;
+
+  return arr[0] * productOfArray(arr.slice(1));
+}
