b5f1d7a878d3b1ce7d3c71293a7f95ebbb5e2e92
fiadaze <fiakantapon@gmail.com>
2025-07-22 00:10:10 +0700

    bubblesort with comparator

--- bubbleSort.js
+++ bubbleSort.js
@@ -13,3 +13,35 @@ function bSort(arr) {
   }
   return arr;
 }
+
+//with comparator
+function comparator2(a, b) {
+  let anum = typeof a === "number";
+  let bnum = typeof b === "number";
+
+  if (anum && bnum) {
+    return a - b;
+  }
+
+  a = a.toLowerCase();
+  b = b.toLowerCase();
+  if (a === b) {
+    return 0;
+  }
+
+  return a > b ? 1 : -1;
+}
+
+function bubbleSort2(arr, compare = comparator2) {
+  for (let i = arr.length; i > 0; i--) {
+    let noSwap = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (compare(arr[j], arr[j + 1]) > 0) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        noSwap = false;
+      }
+    }
+    if (noSwap) break;
+  }
+  return arr;
+}
