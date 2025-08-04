4f25c47249877380a7b6e5067b305c8355725725
fiadaze <fiakantapon@gmail.com>
2025-08-04 23:50:31 +0700

    merge sort mergfunction

--- mergSort.js
+++ mergSort.js
@@ -1,25 +1,24 @@
-function mergeSort(arr) {
-  if (arr.length <= 1) return arr;
+function merge(arr1, arr2) {
+  let result = [];
+  let i = 0;
+  let j = 0;
 
-  const mid = Math.floor(arr.length / 2);
-  const left = mergeSort(arr.slice(0, mid));
-  const right = mergeSort(arr.slice(mid));
-
-  return merge(left, right);
-}
-
-function merge(left, right) {
-  let result = [],
-    i = 0,
-    j = 0;
-
-  while (i < left.length && j < right.length) {
-    if (left[i] < right[j]) {
-      result.push(left[i++]);
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      result.push(arr1[i]);
+      i++;
     } else {
-      result.push(right[j++]);
+      result.push(arr2[j]);
+      j++;
     }
   }
-
-  return result.concat(left.slice(i)).concat(right.slice(j));
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+  }
+  return result;
 }
