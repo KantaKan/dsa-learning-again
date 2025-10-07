55a79bb507885757519b5300d208416c245219d7
KantaKan <fiakantapon@gmail.com>
2025-10-07 23:47:02 +0700

    fixed some bug in code

--- linked-list/singly-linked-list.js
+++ linked-list/singly-linked-list.js
@@ -15,10 +15,15 @@ class SinglyLinkedList {
   }
 
   push(val) {
-    let newNode = new Naode(val);
+    let newNode = new Node(val);
     if (!this.head) {
       this.head = newNode;
       this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
+    this.length++;
+    return this;
   }
 }
