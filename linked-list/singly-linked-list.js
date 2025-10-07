96bce0dcac0adb01905fd5a056b5cfe68054a274
KantaKan <fiakantapon@gmail.com>
2025-10-08 04:37:52 +0700

    feat(linkedlist):added unshift() method

--- linked-list/singly-linked-list.js
+++ linked-list/singly-linked-list.js
@@ -56,4 +56,16 @@ class SinglyLinkedList {
     }
     return first;
   }
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
