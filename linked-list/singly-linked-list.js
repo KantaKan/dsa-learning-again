26ff059d49dcf12558c2cbf39c1e3eb27b9347f4
KantaKan <fiakantapon@gmail.com>
2025-10-08 12:05:27 +0700

    feat(linkedlist):added get() method to use

--- linked-list/singly-linked-list.js
+++ linked-list/singly-linked-list.js
@@ -68,4 +68,14 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
 }
