18195e473d73ce122ad42ec74d9922c86d7c9292
KantaKan <fiakantapon@gmail.com>
2025-10-10 18:27:13 +0700

    feat(linkedlist):fix code prev doesnt add after push

--- linked-list/doubly-linked-list.js
+++ linked-list/doubly-linked-list.js
@@ -19,6 +19,7 @@ class DoublyLinkedList {
       this.tail = newNode;
     } else {
       this.tail.next = newNode;
+      newNode.prev = this.tail;
       this.tail = newNode;
     }
     this.length++;
