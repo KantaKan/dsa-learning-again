e861cb3027fa8ce83bb47703d40b2f8e96aacfa6
KantaKan <fiakantapon@gmail.com>
2026-01-09 01:38:53 +0700

    added example to stack

--- stack/stackWithArray.js
+++ stack/stackWithArray.js
@@ -1,1 +1,12 @@
 //you can use array to make stack just create an array and use push and pop xdd
+
+//example
+
+const stack = [];
+
+stack.push("google");
+
+stack.push("youtube");
+stack.push("instagram");
+
+stack.pop();
